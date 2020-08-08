8568fbabd5219fc4751fab81a8b5812746ecb6f6
Kirill <qSatan1s@yandex.ru>
2020-08-08 17:07:53 +0300

    privet padval

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
-$(function(){
-
+$(function () {
+  $('.photo__slider').slick({
+    prevArrow: '<button  class="slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
+    nextArrow: '<button  class="slick-next"><img src="images/arrow-right.svg" alt=""></button>',
+  });
 });
